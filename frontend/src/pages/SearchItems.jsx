616f6b4f2e3beaafeffcb2bddb5c8d083d6c733f
robel <robelbiniyam0@gmail.com>
2025-03-12 10:54:16 +0300

    fdsa

--- frontend/src/pages/SearchItems.jsx
+++ frontend/src/pages/SearchItems.jsx
@@ -75,67 +75,68 @@ const SearchItems = () => {
 
         {items.length > 0 && (
           <div className="search-results">
-            {items.map((item) => (
-              <div key={item._id} className="container m-4">
-                <div className="card">
-                  <div className="logo">
-                    <span className="circle c1"></span>
-                    <span className="circle c2"></span>
-                    <span className="circle c3"></span>
-                    <span className="circle c4"></span>
-                    <span className="circle c5">
-                      <i className="fa-solid fa-seedling"></i>
-                    </span>
-                  </div>
-                  <div className="glass">
-                    <div className="content">
-                      <img
-                        src={logo}
-                        alt="Store Logo"
-                        style={{
-                          height: "90px",
-                          width: "90px",
-                          marginTop: "5px",
-                        }}
-                      />
-                      <h1>{item.name}</h1>
-                      <p> ipsum dolor sit amet consectetur, adipisicing elit.</p>
+          {items.map((item) => (
+                <div key={item._id} className="container m-4">
+                  <div className="card" style={{}}>
+                    <div className="logo">
+                      <span className="circle c1"></span>
+                      <span className="circle c2"></span>
+                      <span className="circle c3"></span>
+                      <span className="circle c4"></span>
+                      <span className="circle c5">
+                       <img
+                                     src={logo}
+                                     alt="Store Logo"
+                                     style={{ height: "40px", width: "40px", marginTop: "5px" }}
+                                   />
+                      </span>
                     </div>
-                    <div className="footer">
-                      <div className="social">
-                        <span className="social_icons">
-                          <Link
-                            to={`/items/details/${item._id}`}
-                            className="hover:text-blue-600 transition-colors duration-200 details"
-                          >
-                            <CiSquareInfo />
-                          </Link>
-                        </span>
-                        <span className="social_icons">
-                          <Link
-                            to={`/items/edit/${item._id}`}
-                            className="hover:text-green-600 transition-colors duration-200 edit"
-                          >
-                            <CiEdit />
-                          </Link>
-                        </span>
-                        <span className="social_icons">
-                          <Link
-                            to={`/items/delete/${item._id}`}
-                            className="hover:text-red-600 transition-colors duration-200 delete"
-                          >
-                            <MdDelete />
-                          </Link>
-                        </span>
+                    <div className="glass"   style={{
+              transformStyle: 'preserve-3d',
+              position: 'absolute',
+              inset: '8px',
+              backgroundImage: `linear-gradient(#ffffff59, #ffffffd0), url(${item.image})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
+              borderRadius: '55px',
+              borderTopRightRadius: '100%',
+              borderLeft: '1px solid #fff',
+              borderBottom: '1px solid white',
+              transition: '0.5s ease-in-out',
+              transform: 'translate3d(0, 0, 25px)',
+          }}>
+                      <div className="content">
+                      
+                        <h1 style={{marginTop:"60px ", color:"#00c37b"}}>{item.name}</h1>
+                        <p> </p>
                       </div>
-                      <div className="link">
-                        Read More
+                      <div className="footer">
+                        <div className="social"> 
+                          <span className="social_icons">
+                            <Link to={`/items/details/${item._id}`} className="hover:text-blue-600 transition-colors duration-200 details">
+                              <CiSquareInfo />
+                            </Link>
+                          </span>
+                          <span className="social_icons">
+                            <Link to={`/items/edit/${item._id}`} className="hover:text-green-600 transition-colors duration-200 edit">
+                              <CiEdit />
+                            </Link>
+                          </span>
+                          <span className="social_icons">
+                            <Link to={`/items/delete/${item._id}`} className="hover:text-red-600 transition-colors duration-200 delete">
+                              <MdDelete />
+                            </Link>
+                          </span>
+                        </div>
+                        <div className="link">
+                          Read More
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
+              ))}
           </div>
         )}
       </div>
